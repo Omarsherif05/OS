11fba50e7107d7a0ccef0d95279b09f7aad2f87a
mourad2213 <ahmed.mourad@gmail.com>
2024-12-03 20:33:19 +0200

    ssss

--- ReadyQueue.java
+++ ReadyQueue.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ReadyQueue {
+//package PACKAGE_NAME;
+public class ReadyQueue {
+ private Queue<Process> ReadyQueue = new LinkedList<>();
+
+    public void addProces(Process i){
+        ReadyQueue.add(i);
+    }
+    public Process removeProces(){
+        return  ReadyQueue.poll();
+
+    }
+    public boolean isEmpty(){
+        return ReadyQueue.isEmpty();
+    }
+
 }
