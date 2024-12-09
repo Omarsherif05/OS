6675ce55b4069f2fd674ad083460fdfaa7183e90
Mostafa <mostafa.soliman@student.giu-uni.de>
2024-12-09 18:57:48 +0200

    I added the memboundaries into the PCB constructor and fixed every error in terms of parameters

--- Project/PCB.java
+++ Project/PCB.java
@@ -4,11 +4,14 @@ public class PCB {
     private final int processId;
     private int instructionCount;
     private int programCounter;
+    private int[] memoryBoundaries = new int[2];        //its in the docx
 
-    public PCB(int processId, int instructionCount) {
+    public PCB(int processId, int instructionCount, int boundaryOne, int boundaryTwo) {
         this.processId = processId;
         this.instructionCount = instructionCount;
         this.programCounter = 0;
+        this.memoryBoundaries[0] = boundaryOne;         //its in the docx
+        this.memoryBoundaries[1] = boundaryTwo;         //its in the docx
     }
 
     public int getProcessId() {
